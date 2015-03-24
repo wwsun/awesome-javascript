4329c49705cfa8ad9e14b46e6d7d5441dcead442
Weiwei SUN <ww.sun@outlook.com>
2015-03-24 09:47:06 +0800

    test random number generator

--- src/test/java/me/wwsun/ArrayTest.java
+++ src/test/java/me/wwsun/ArrayTest.java
@@ -5,6 +5,9 @@ import me.wwsun.array.GameEntry;
 import me.wwsun.array.InsertionSort;
 import me.wwsun.array.ScoreBoard;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class ArrayTest extends TestCase {
     public void testArrays() {
         ScoreBoard sb = new ScoreBoard(5); // init a score board with 5 positions
@@ -28,5 +31,25 @@ public class ArrayTest extends TestCase {
 
         InsertionSort.insertionSort(a);
         System.out.println(java.util.Arrays.toString(a));
+    }
+
+    public void testRandomNumberGenerator() {
+        int data[] = new int[10];
+
+        Random rand = new Random();
+        rand.setSeed(System.currentTimeMillis()); // use current time as a seed
+
+        // fill the data array with presudo-random numbers from 0 to 99
+        for (int i = 0; i < data.length; i++) {
+            data[i] = rand.nextInt(100);
+        }
+
+        int[] origin = Arrays.copyOf(data, data.length); //make a copy of data array
+        System.out.println("Arrays equal before sort:\t" + Arrays.equals(data, origin));
+        Arrays.sort(data);
+        System.out.println("Arrays equal after sort:\t" + Arrays.equals(data, origin));
+        System.out.println("Origin:\t" + Arrays.toString(origin));
+        System.out.println("Data:\t" + Arrays.toString(data));
+
     }
 }
