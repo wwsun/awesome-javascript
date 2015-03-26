f7d9a95691d3b4fc88875a154ca178ed94946cd2
Weiwei SUN <ww.sun@outlook.com>
2015-03-26 19:54:48 +0800

    test disk usage

--- src/test/java/me/wwsun/RecursionTest.java
+++ src/test/java/me/wwsun/RecursionTest.java
@@ -1,7 +1,10 @@
 package me.wwsun;
 
 import junit.framework.TestCase;
+import me.wwsun.recursion.DiskSpace;
 import me.wwsun.recursion.Factorial;
+
+import java.io.File;
 
 /**
  * Created by Weiwei on 2015/3/26.
@@ -13,5 +16,10 @@ public class RecursionTest extends TestCase {
         System.out.println("Factorial("+n+")= " + Factorial.factorial(n));
         assertEquals(1, Factorial.factorial(0));
         assertEquals(2, Factorial.factorial(2));
+    }
+
+    public void testDiskUsage() {
+        String path = "d:\\Movie";
+        DiskSpace.diskUsage(new File(path));
     }
 }
