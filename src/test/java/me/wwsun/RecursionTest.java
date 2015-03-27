2cc886d9c79351849c8be43ff4e9cf68a97d97a7
Weiwei SUN <ww.sun@outlook.com>
2015-03-27 09:54:12 +0800

    test case update

--- src/test/java/me/wwsun/RecursionTest.java
+++ src/test/java/me/wwsun/RecursionTest.java
@@ -1,6 +1,8 @@
 package me.wwsun;
 
 import junit.framework.TestCase;
+import me.wwsun.recursion.ArrayReverse;
+import me.wwsun.recursion.ArraySum;
 import me.wwsun.recursion.DiskSpace;
 import me.wwsun.recursion.Factorial;
 
@@ -21,5 +23,22 @@ public class RecursionTest extends TestCase {
     public void testDiskUsage() {
         String path = "d:\\Movie";
         DiskSpace.diskUsage(new File(path));
+    }
+
+    public void testArraySum() {
+        int data[] = {1, 4, 5, 20, 23};
+        int sum = ArraySum.linearSum(data, data.length);
+        int sum2 = ArraySum.binarySum(data, 0, data.length-1);
+        System.out.println(sum);
+        System.out.println(sum2);
+    }
+
+    public void testArrayReverse() {
+        int data[] = {1, 4, 5, 20, 23};
+        ArrayReverse.reverseArray(data, 0, data.length-1);
+
+        for (int i : data) {
+            System.out.print(i + " ");
+        }
     }
 }
